3c987a3354c02e49ff945300d7ea1ee8745261da
MilicaS99 <m.suknovic99@gmail.com>
2022-02-04 23:17:25 +0100

    Dodat CSS

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -7,10 +7,10 @@ const ContactCard=(props)=>{
       <div className="item">
         <img className="ui avatar image"src={user}alt="user"/>
       <div className="content">
-        <Link to={{pathname:`/contact/${id}`,state:{contact:props.contact}}}>
+       
         <div className="header">{name}</div>
         <div>{email}</div> 
-        </Link>       
+      
          </div>
       <i 
       className="trash alternate outline icon"
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,8 @@ import {Link} from "react-router-dom";
 const Header = () =>{
 
   return(
-    <div className="ui fixed menu">
+    <div>
       <div className="ui container center">
-        
-        <h1>Contact Manager</h1>
    
       </div>
     </div>
